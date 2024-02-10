d2b5174991a65318bda4b44acca34dc042a80337
Daizo-1207 <daizo.ogawa@gmail.coom>
2024-02-10 14:52:58 +0900

    Delete title

--- database/migrations/2024_02_09_105944_create_reviews_table.php
+++ database/migrations/2024_02_09_105944_create_reviews_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->text('title');
             $table->text('content');
             $table->integer('product_id');
             $table->integer('user_id');
